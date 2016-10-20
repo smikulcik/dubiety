942e20c9382170775487bddaea6d817d3cc69fdd
Mikulcik <simon_mikulcik@mymail.eku.edu>
2016-10-20 17:34:20 -0400

    Add sessions and styled chat

--- server.js
+++ server.js
@@ -3,8 +3,26 @@ var handlebars = require('handlebars'),
 fs = require('fs');
 
 var app = express();
+var expressWs = require('express-ws')(app);
 
 var PORT = 3000;
+
+var antons = {};
+
+var Anton = function(ws){
+	this.name = 'Anton';
+	this.ws = ws;
+};
+Anton.prototype.handleMessage = function(message){
+	this.ws.send(message.toUpperCase());
+};
+
+app.ws('/ws', function(ws, req) {
+	var anton = new Anton(ws);
+	ws.on('message', function(msg) {
+		anton.handleMessage(msg);
+	});
+});
 
 app.get('/', function(req, res) {
 	fs.readFile('./html/index.html', 'utf-8', function(err, src){
@@ -19,4 +37,3 @@ app.use(express.static('static'));
 app.listen(PORT, function() {
 	console.log('Started Listening!');
 });
-
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,26 +1,34 @@
 (function($){
 
-	$.fn.Chat = function(username) {
+	var scrollToBottom = function(el){
+		el.get(0).scrollTop = el.get(0).scrollHeight;
+	}
+
+	$.fn.Chat = function(username, onchatinput) {
 		this.append("<div class='history'></div>");
 		this.append("<form><input /><button type=submit>Send</button></form>");
 
 		var input = this.find('input');
 		var button = this.find('button');
-		var chathist = this.find('.history');
+		var history = this.find('.history');
 
 		button.click(function(event){
 			event.preventDefault();
 			var message = input.val();
-			chathist.append('<div class=\'me\'><div class=\'username\'>' + 
+			history.append('<div class=\'me\'><div class=\'username\'>' +
 				username + '</div>' + message + '</div>');
 			input.val("");
+			scrollToBottom(history);
+			if(onchatinput !== undefined && typeof(onchatinput) === 'function')
+				onchatinput(message);
 		});
 
 		$.fn.Chat.receiveMsg = function(message, sender){
-			chathist.append('<div class=\'other\'><div class=\'username\'>' + 
+			history.append('<div class=\'other\'><div class=\'username\'>' +
 				sender + '</div><div class=message>' + message + '</div></div>');
+
+			scrollToBottom(history);
 		};
 	};
 
 }(jQuery));
-
